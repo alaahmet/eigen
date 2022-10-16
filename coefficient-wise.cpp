d411150212b40a4792b3aab70b03a76fa792e08c
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2022-10-16 14:57:24 +0200

    Add files via upload

---
+++ coefficient-wise.cpp
@@ -0,0 +1,23 @@
+// https://eigen.tuxfamily.org/dox/group__TutorialArrayClass.html
+#include <bits/stdc++.h>
+#include <eigen3/Eigen/Dense>
+int main(){ 
+    
+    Eigen::VectorXd v(6);
+    v << 2,3,1,6,4,5;
+    std::cout << v.transpose() << std::endl;
+
+    Eigen::VectorXd dinv = (1/v.array()).matrix();
+    std::cout << dinv.transpose() << std::endl;
+
+    Eigen::VectorXd Mul = dinv.cwiseProduct(v);
+    std::cout << Mul.transpose() << std::endl;
+
+}
+/*
+
+2 3 1 6 4 5
+0.5 0.333333 1 0.166667 0.25 0.2
+1 1 1 1 1 1
+
+*/
