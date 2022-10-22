eda94836b30e42da7f1415370a042d607d716cc6
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2022-10-22 10:10:25 +0200

    Update coefficient-wise.cpp

--- coefficient-wise.cpp
+++ coefficient-wise.cpp
@@ -13,11 +13,14 @@ int main(){
     Eigen::VectorXd Mul = dinv.cwiseProduct(v);
     std::cout << Mul.transpose() << std::endl;
 
+    Eigen::VectorXd logv = v.array().log();
+    std::cout << logv.transpose() << std::endl;
 }
 /*
 
 2 3 1 6 4 5
 0.5 0.333333 1 0.166667 0.25 0.2
 1 1 1 1 1 1
+0.693147  1.09861        0  1.79176  1.38629  1.60944
 
 */
