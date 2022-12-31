0d33ab7ab31fdad6f217fe839e27fddf1457f62b
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2022-12-31 11:17:47 +0100

    Update GivensRotation.cpp

--- GivensRotation.cpp
+++ GivensRotation.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     Eigen::VectorXd v = Eigen::VectorXd(5);
     v << 13,8,4,92,3223; // vector
-    // annihilate 3rd element of vector
+    // annihilate 4th element of vector
     double c = 13/(sqrt(13*13+92*92));
     double s = 92/(sqrt(13*13+92*92));
     Eigen::MatrixXd A = Eigen::MatrixXd::Identity(5,5);
